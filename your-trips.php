84614a0bdf7ee7f1ad7b699c37c0d1102ecc7121
Mike Hoffman <hoffman@learningbird.com>
2015-09-26 15:29:53 -0400

    Added some trip management UI

---
+++ your-trips.php
@@ -0,0 +1,31 @@
+<div class="row">
+	<div class="col-sm-4">
+		<h1>CashMoney</h1>
+		<p><a class="add-trip" href="#">Add a new trip</a></p>
+		<div class="list-group past-trips"></div>
+	</div>
+</div>
+
+<script type="handlerbars-template" id="trip-item-template">
+	{{#each this}}
+		<a href="#" class="list-group-item">{{name}}</a>
+	{{/each}}
+</script>
+
+<script>
+	$(function() {
+		var fakeTripData = [
+			{ id: 1, name: "New Zealand Ski Trip" },
+			{ id: 2, name: "Last week at the pub" },
+			{ id: 3, name: "International Space Station" },
+		];	
+		
+		$('.past-trips').html(renderTemplate('trip-item-template', fakeTripData));
+		
+		$('.add-trip').click(function(e) {
+			e.preventDefault();
+			
+			$('.past-trips').prepend('<input class="list-group-item"/>')
+		});
+	})
+</script>
